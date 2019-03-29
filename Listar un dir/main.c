7f6f7d4daeff99221775ed6bc88dbac8ae8d57ea
davidssantoss <davidsteven.santos9@gmail.com>
2019-03-28 22:20:36 -0500

    .

--- Listar un dir/main.c
+++ Listar un dir/main.c
@@ -6,28 +6,40 @@
 *
 *@author: David s.
 *
-**/
+*/
 
 int main(int argc, char *argv[]) {
+	/* Arreglo de caracteres en donde se introduce la ruta del directorio*/
 	char dirn[50];
+	/*Un tipo que de dato que representa la secuencia/flujo de un directorio*/
     DIR *dir = NULL;
+    /** Estructura  del tipo dirent
+    
+    *struct dirent{
+	*	long		d_ino;		 Always zero. 
+	*	unsigned short	d_reclen;	 Always zero. 
+	*	unsigned short	d_namlen;	 Length of name in d_name. 
+	*	char		d_name[260];  [FILENAME_MAX] */ /* File name. 
+	*}; 
+	*/
     struct dirent *drnt = NULL;
 
     printf("Digite la ruta del directorio: ");
+    /*obtiene el valor digitado de la ruta*/
     gets(dirn);
     
-
-    dir=opendir(dirn);
+	/*utiliza la funcion opendir para abrir la ruta de un directorio */
+    dir=opendir(dirn); /*asigna dir la ruta ya digitada*/
     if(dir){
         printf("Salida:\n");
         while(drnt = readdir(dir)){
-            printf("%-10s\t", drnt->d_name, "\n");
+            printf("%-10s\t", drnt->d_name, "\n"); /* imprime los archivos del directorio*/
         }
         closedir(dir);
     }
     else{
         printf("No se puede abrir el archivo '%s'\n", dirn, "\n");
     }
-    system ("PAUSE");
+    system ("\n PAUSE");
 	return 0;
 }
