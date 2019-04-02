f1e6223e57d4b07b87e005f68e28dd6c3ad8bede
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2019-04-02 11:57:10 -0500

    cosas

--- Listar un dir/main.c
+++ Listar un dir/main.c
@@ -13,30 +13,31 @@ int main(int argc, char *argv[]) {
 	char dirn[50];
 	/*Un tipo que de dato que representa la secuencia/flujo de un directorio*/
     DIR *dir = NULL;
-    void toUpperCase(char *archivo);
+    char nombreArchivo[50];
+		void renameFiles(char *archivo, char *dirn);
+    const char * toUpperCase(char *archivo);
     /** Estructura  del tipo dirent
-    
+
     *struct dirent{
-	*	long		d_ino;		 Always zero. 
-	*	unsigned short	d_reclen;	 Always zero. 
-	*	unsigned short	d_namlen;	 Length of name in d_name. 
-	*	char		d_name[260];  [FILENAME_MAX] */ /* File name. 
-	*}; 
+	*	long		d_ino;		 Always zero.
+	*	unsigned short	d_reclen;	 Always zero.
+	*	unsigned short	d_namlen;	 Length of name in d_name.
+	*	char		d_name[260];  [FILENAME_MAX] */ /* File name.
+	*};
 	*/
     struct dirent *drnt = NULL;
 
     printf("Digite la ruta del directorio: ");
     /*obtiene el valor digitado de la ruta*/
     gets(dirn);
-    
+
 	/*utiliza la funcion opendir para abrir la ruta de un directorio */
     dir=opendir(dirn); /*asigna dir la ruta ya digitada*/
     if(dir){
         printf("Salida:\n");
         while(drnt = readdir(dir)){
             printf("%-10s\t", drnt->d_name, "\n"); /* imprime los archivos del directorio*/
-            /*toUpperCase(drnt->d_name);*/
-            
+            renameFiles(drnt->d_name, &dirn);
         }
         closedir(dir);
     }
@@ -46,16 +47,30 @@ int main(int argc, char *argv[]) {
     system ("\n PAUSE");
 	return 0;
 }
-/*
-void toUpperCase(char *archivo){
+
+void renameFiles(char *archivo, char *dirn)
+{
 	FILE *file;
-	
 	file = fopen(archivo, "w");
-	if(rename(archivo,"archivo2.txt")==0) /* Renombramos el archivo*//*
+	char *nuevoNombre = toUpperCase(archivo);
+	printf("archivo en mayus: %s ", *nuevoNombre);
+	if(rename(archivo, *nuevoNombre)==0) /* Renombramos el archivo */
         printf("El archivo se renombro satisfactoriamente\n");
     else
         printf("No se pudo renombrar el archivo\n");
-		
-	
-	
-}*/
+}
+
+
+const char * toUpperCase(char *file_name)
+{
+  char * name;
+  name = strtok(file_name,":");
+
+  // Convert to upper case
+  char *s = name;
+  while (*s) {
+    *s = toupper((unsigned char) *s);
+    s++;
+  }
+	return name;
+}
